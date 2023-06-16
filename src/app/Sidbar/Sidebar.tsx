63168dd58c358fb54bd66a4f0e8d077ec9744508
HANNSTAR\ignaciochen <ignaciochen@hannstar.com>
2023-06-16 15:08:12 +0800

    feat: side icon click action

--- src/app/Sidbar/Sidebar.tsx
+++ src/app/Sidbar/Sidebar.tsx
@@ -1,10 +1,35 @@
+'use client'
+
 import './Sidebar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faFire, faPersonWalking, faCoins, faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons';
 import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import React, { useState } from "react";
 
 export default function Sidebar() {
+  const menusArray = [
+    {
+      name: "體育",
+      icon: faPersonWalking,
+      star: faStarRegular,
+      isFavorite: false
+    },
+    {
+      name: "區塊鏈",
+      icon: faCoins,
+      star: faStarRegular,
+      isFavorite: false
+    }
+  ];
+  const [menus, setCurrentStarIcon] = useState(menusArray);
+  const favoriteClick = async (index: number) => {
+    const updateMenus = [...menus];
+    updateMenus[index].star = updateMenus[index].isFavorite ? faStarRegular : faStarSolid;
+    updateMenus[index].isFavorite = !updateMenus[index].isFavorite;
+    setCurrentStarIcon(updateMenus);
+  }
+
   return (
     <aside>
       <div className="side">
@@ -18,21 +43,24 @@ export default function Sidebar() {
             <FontAwesomeIcon icon={faFire} size="lg" />
             <span>熱門</span>
           </a>
-          <div className="menu">收藏的</div>
-          <div className="menu-collapse-item">
-            <a href="#"></a>
-          </div>
+          <div className="menu">喜愛的</div>
+            {menus.map((menu, index) => {
+              if (menu.isFavorite) {
+                return (<div key={index} className="menu-collapse-item">
+                  <FontAwesomeIcon icon={menu.icon} size="lg"/>
+                  <a href="#">{menu.name}</a>
+                  <FontAwesomeIcon icon={menu.star} size="lg" onClick={() => favoriteClick(index)}/>
+                </div>);
+              }
+            })}
           <div className="menu">討論區</div>
-          <div className="menu-collapse-item">
-            <FontAwesomeIcon icon={faPersonWalking} size="lg" />
-            <a href="#">體育</a>
-            <FontAwesomeIcon icon={faStarRegular} size="lg" />
-          </div>
-          <div className="menu-collapse-item">
-            <FontAwesomeIcon icon={faCoins} size="lg" />
-            <a href="#">區塊鏈</a>
-            <FontAwesomeIcon icon={faStarRegular} size="lg" />
-          </div>
+            {menus.map((menu, index) => (
+              <div key={index} className="menu-collapse-item">
+                <FontAwesomeIcon icon={menu.icon} size="lg" />
+                <a href="#">{menu.name}</a>
+                <FontAwesomeIcon icon={menu.star} size="lg" onClick={() => favoriteClick(index)} />
+              </div>
+            ))}
         </div>
         <hr />
         <div className="side-footer">
